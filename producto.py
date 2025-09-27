eee5bdf361d35a13ee3109a8f61d20e131ce28b0
EdisonM12 <eddy.matute2004@gmail.com>
2025-09-27 17:21:01 -0500

    primera clase

---
+++ producto.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+@dataclass
+class Producto:
+    id: int
+    nombre: str
+    precio: int
+    cant: int
+    stock: int
+    stock_minimo: int
+
+    def agregar_stock(self, cantidad : int):
+         if cantidad < 0:
+             raise ValueError('Coloque una cantidad correcta')
+
+         self.stock += cantidad
+
+    def validaciones_stcok(self):
+         if self.stock <= self.stock_minimo:
+             raise ValueError("colocar mas stock")
+
+    def disminuir_stock(self, cantidad: int):
+         if cantidad < 0:
+             raise ValueError('Coloque una cantidad positiva')
+         self.stock -= cantidad
+
+
+    def validacion_Venta(self, cantidad: int):
+         if(cantidad <= 0):
+             raise ValueError("debe agregar una cantidad valida")
+         if cantidad > self.stock:
+             raise ValueError("no hay suficiente inventario")
+         self.disminuir_stock(cantidad)
+
